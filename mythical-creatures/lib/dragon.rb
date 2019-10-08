693b5474fae82a3e4935534ac92cae9e134e6009
Travis Borgsmiller <travisborgsmiller@gmail.com>
2019-10-08 14:26:26 -0600

    Add dragon for mythical creatures

--- mythical-creatures/lib/dragon.rb
+++ mythical-creatures/lib/dragon.rb
@@ -3,20 +3,26 @@ class Dragon
 attr_accessor :name,
               :color,
               :rider,
-              :hungry
+              :hungry,
+              :counter
 
   def initialize(name, color, rider_name)
     @name = name
     @color = color
     @rider = rider_name
-    @hungry
+    @hungry = true
+    @counter = 0
   end
 
   def hungry?
-    @hungry = true
+    @hungry
   end
 
   def eat
-
+    @counter += 1
+    if @counter == 3
+      @hungry = false
+    end
+    @hungry
   end
 end
